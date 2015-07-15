7d318e858f964550146b87fc421207431b69c3e6
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-07-15 14:01:30 +0300

    Fix rows not being inited properly

--- src/gui/template-content.rkt
+++ src/gui/template-content.rkt
@@ -76,7 +76,7 @@
         (cons (new section%
                    [parent this]
                    [section-label name]
-                   [rows '(rows (row ()))])))
+                   [rows '(rows (row))])))
       
       (set-children inner-sections))
 
