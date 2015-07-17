c0e36fca49a6214cb3995333810258a3e1404f33
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-07-17 18:54:11 +0300

    Remove add-button functionality from add-frame

--- src/gui/add-frame.rkt
+++ src/gui/add-frame.rkt
@@ -50,18 +50,6 @@
            [parent this]
            [label "Add section"]))
 
-    (define add-button-button
-      (new button%
-           [label "Add"]
-           [parent this]
-           [callback
-             (lambda (button event)
-               (case (send type-radio-box get-selection)
-                 [(0) (send add-button-dialog show #t)]
-                 [(1) (send add-row-dialog show #t)]
-                 [(2) (send add-section-dialog show #t)]
-                 [else #f]))]))
-    
     (define/public
       (add-done)
       
